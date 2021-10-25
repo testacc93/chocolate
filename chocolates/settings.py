987228144eef683417c6624c83e706caa3cc325b
Ankur K <ankur@git.com>
2021-10-25 11:21:03 +0530

    debug

--- chocolates/settings.py
+++ chocolates/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '#+ytf-v@iu+-3u@p^oy^()o1pyv8s@g%8!7#**^4(m-=6-od+w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['etrunk.herokuapp.com', '127.0.0.1']
 
